bd2a9238d92d82869556cd2bb1fa00483f433675
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-09-12 11:42:33 -0300

    foiii

--- loginExe.php
+++ loginExe.php
@@ -1,4 +1,4 @@
-<php>
+<?php
     include('include/conexao.php');
     $email = $_POST['email'];
     $senha = $_POST['senha'];
@@ -7,6 +7,21 @@
     $result = mysqli_query($con,$sql);
     if(mysqli_num_rows($result) > 0)
     {
-        $row = mysqli_
+        $row = mysqli_fetch_array($result);
+        if($row['senha'] == $senha)
+        {
+            include('inicia_sessao.php');
+            $_SESSION['login'] = $row;
+            // volta para a apagina inicial
+            header('Location: index.php');
+        }else
+        {
+            echo "<h1>Senha inválida!
+            Sua senha é " . $row['senha'] . "</h1>";
+        }
+
+    }else
+    {
+        echo "<h1>Usuário não encontrado!</h1>";
     }
->
+?>
