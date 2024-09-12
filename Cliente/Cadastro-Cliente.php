b8265f86c175bbf014d37050e14025727c7aea86
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-09-12 10:12:04 -0300

    foiii

--- Cliente/Cadastro-Cliente.php
+++ Cliente/Cadastro-Cliente.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../CSS/Style.css">
     <title>Document</title>
 </head>
 
 <body>
     <?php
-    include('../includes/conexao.php');
+    include('../include/conexao.php');
     $nome = $_POST['nome'];
     $senha = $_POST['senha'];
     $email = $_POST['email'];
