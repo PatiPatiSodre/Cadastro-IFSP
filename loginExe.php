9e6714fcec536fc54a5a4f8402c999ee009db6e5
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-09-12 10:48:25 -0300

    funciona

--- loginExe.php
+++ loginExe.php
@@ -1,4 +1,12 @@
 <php>
-    include('conexao.php');
-    
+    include('include/conexao.php');
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $sql   = "SELECT * FROM cliente
+            WHERE email = '$email'";
+    $result = mysqli_query($con,$sql);
+    if(mysqli_num_rows($result) > 0)
+    {
+        $row = mysqli_
+    }
 </php>
