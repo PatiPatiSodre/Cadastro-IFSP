719f24cf9f2c475665aa1a6d6182824a8f0ab57d
patricia <patricia.sodre@aluno.ifsp.edu.br>
2024-09-12 11:03:49 -0300

    foiii

--- include/conexao.php
+++ include/conexao.php
@@ -3,7 +3,7 @@ $hostname = 'localhost';
 $username = 'root';
 $password = '';
 $database = 'IFSP';
-$port = 3307;
+$port = 3306;
 $con = mysqli_connect($hostname, $username, $password, $database, $port);
 if (mysqli_connect_errno()) {
     printf("Erro conexão: %s", mysqli_connect_error());
